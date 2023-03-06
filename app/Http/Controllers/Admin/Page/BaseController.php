a1bf1fa53c5a9f4cf7f7fa57989e662b25903996
uncleA <lotar73@gmail.com>
2023-03-06 14:27:46 +0300

    Исправление ошибок 2

--- app/Http/Controllers/Admin/Page/BaseController.php
+++ app/Http/Controllers/Admin/Page/BaseController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\Admin\Page;
 
 use App\Http\Controllers\Controller;
-use App\Service\MainService;
+use App\Service\PageService;
 use Illuminate\Http\Request;
 
 class BaseController extends Controller
 {
     public $service;
-    public function __construct(MainService $service)
+    public function __construct(PageService $service)
     {
         $this->service = $service;
 
