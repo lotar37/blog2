df2b714d82a683f86a62b61245bea0082276c150
Home comp <lotar37@gmail.com>
2023-03-14 12:13:36 +0300

    Страница поста

--- resources/views/main/post_show.blade.php
+++ resources/views/main/post_show.blade.php
@@ -5,19 +5,20 @@
         <div class="container">
             <h1 class="edica-page-title" data-aos="fade-up">{{ $post->title }}</h1>
             <p class="edica-blog-post-meta" data-aos="fade-up" data-aos-delay="200">{{ $post->date." ".$post->time }}</p>
+
             <section class="blog-post-featured-img" data-aos="fade-up" data-aos-delay="300">
                 <img src="{{  asset("storage/" . $post->main_image) }}" alt="{{$post->main_image}}" class="w-100 ">
             </section>
             <section class="post-content">
                 <div class="row">
                     <div class="col-lg-9 mx-auto" data-aos="fade-up">
-                        <h2 class="mb-4" data-aos="fade-up">Резюме</h2>
-                        {!! $post->content  !!}
+
+                        <h4>{!! $post->content  !!}</h4>
                     </div>
                     @if($post->report)
 
                     <div class="col-lg-9 mx-auto" data-aos="fade-up">
-                        <h2 class="mb-4" data-aos="fade-up">Отчет</h2>
+{{--                        <h2 class="mb-4" data-aos="fade-up">Отчет</h2>--}}
                         {!! $post->report  !!}
                     </div>
                     @endif
