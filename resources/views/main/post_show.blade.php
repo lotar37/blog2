1b5bfc5431a879201dd0269d9f4620d52cf64695
uncleA <lotar73@gmail.com>
2023-03-15 12:00:18 +0300

    форма мероприятия

--- resources/views/main/post_show.blade.php
+++ resources/views/main/post_show.blade.php
@@ -3,38 +3,47 @@
 @section('content')
     <main class="blog blog-post">
         <div class="container">
-            <h1 class="edica-page-title" data-aos="fade-up">{{ $post->title }}</h1>
+            <h2 class="edica-page-title" data-aos="fade-up">{{ $post->title }}</h2>
             <p class="edica-blog-post-meta" data-aos="fade-up" data-aos-delay="200">{{ $post->date." ".$post->time }}</p>
 
-            <section class="blog-post-featured-img" data-aos="fade-up" data-aos-delay="300">
+            <section class="blog-post-featured-img edica-landing-section edica-landing-blog" data-aos="fade-up" data-aos-delay="300">
                 <div class="row">
                     <div class="col-md-9 mx-auto" data-aos="fade-up">
-                <img src="{{  asset("storage/" . $post->main_image) }}" alt="{{$post->main_image}}" class="w-100">
+                        <div class="row">
+
+                            <div class="col-md-10 mx-auto" data-aos="fade-up">
+                        <img src="{{  asset("storage/" . $post->main_image) }}" alt="{{$post->main_image}}" class="w-100">
+                            </div>
+                        </div>
+
+
+                        <div class="row">
+                            <div class="col-md-10 mx-auto" data-aos="fade-up">
+                                <h4>{!! $post->content  !!}</h4>
+                            </div>
+                        </div>
+                        <div class="row">
+
+
+                            <div class="col-md-10 ml-auto" data-aos="fade-up">
+
+                                @if($post->report)
+                                {!! $post->report  !!}
+                                @endif
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <div class="col-md-10 mx-auto" data-aos="fade-up">
+                                <img src="{{  asset("storage/" . $post->preview_image) }}" alt="{{$post->main_image}}" class="w-100">
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </section>
-            <section class="edica-landing-section edica-landing-blog">
-{{--            <section class="post-content">--}}
-                <div class="row">
-                    <div class="col-md-9 mx-auto" data-aos="fade-up">
-                        <h4>{!! $post->content  !!}</h4>
-                    </div>
-                </div>
-                <div class="row">
-
-
-                    <div class="col-md-7 ml-auto" data-aos="fade-up">
-
-                        @if($post->report)
-                        {!! $post->report  !!}
-                        @endif
-                    </div>
-
-                    <div class="col-md-2 sidebar mr-auto" data-aos="fade-up" >
+                    <div class="col-md-3 sidebar mr-auto" data-aos="fade-up" >
+                        <h4 class="mx-auto">Ещё мероприятия</h4>
                         @include('main.includes.random_post')
                     </div>
                 </div>
-
             </section>
         </div>
     </main>
