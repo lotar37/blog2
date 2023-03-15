1be7069d9e6f857b6d7e6c05504a1e5998f90c86
uncleA <lotar73@gmail.com>
2023-03-15 12:12:19 +0300

    форма мероприятия правка ошибок

--- resources/views/main/post_show.blade.php
+++ resources/views/main/post_show.blade.php
@@ -25,7 +25,7 @@
                         <div class="row">
 
 
-                            <div class="col-md-10 ml-auto" data-aos="fade-up">
+                            <div class="col-md-10 mx-auto" data-aos="fade-up">
 
                                 @if($post->report)
                                 {!! $post->report  !!}
