4a08c0aed69905f1f16cf45992565c24755b8a0c
uncleA <lotar73@gmail.com>
2023-03-21 12:47:49 +0300

    Главная страница домашних заданий

--- resources/views/main/homeworks/index.blade.php
+++ resources/views/main/homeworks/index.blade.php
@@ -19,32 +19,29 @@
                 </section>
             <section class="edica-about-faq py-0 card-body py-0 " >
                 <div class="row">
-                    <div class="col-6">
 
-                        <div class="accordion" id="edicaAboutFaqCollapse">
+
                             @foreach($workload as $s_key=>$subject)
+                        <div class="col-6 mb-1">
                                 <div class="card aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
                                     <div class="card-header" role="tab" id="edicaAboutFaq{{ $s_key }}">
                                         <h5 class="mb-0">
-                                            <a data-toggle="collapse" data-parent="#edicaAboutFaqCollapse"
-                                               href="#edicaAboutFaqContent{{ $s_key }}" aria-expanded="false"
-                                               aria-controls="edicaAboutFaqContent{{ $s_key }}" class="collapsed">
+
                                                 {{ $data['subjects']->find($s_key)->name }}
-                                            </a>
+
                                         </h5>
                                     </div>
-                                    <div id="edicaAboutFaqContent{{ $s_key }}" class="in collapse" role="tabpanel" aria-labelledby="edicaAboutFaq{{ $s_key }}" style="">
                                         <div class="card-body">
                                             <div class="btn-group  btn-group-md btn-group-justified col-6">
                                                 @foreach($subject as $c_key=>$class)
                                                     <a class="btn btn-outline-dark" href="{{route("main.homework.show",[$data['subjects']->find($s_key)->id,$data['classes']->find($c_key)->id])}}">{{ $data['classes']->find($c_key)->name }}</a>
                                                 @endforeach
                                             </div>
-                                        </div>
                                     </div>
                                 </div>
-                            @endforeach
                         </div>
+                            @endforeach
+
                     </div>
                 </div>
             </section>
