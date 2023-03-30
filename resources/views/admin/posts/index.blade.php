6a49a6fe74cb2654cd059092c0e3be352c52e9cf
uncleA <lotar73@gmail.com>
2023-03-30 18:05:23 +0300

    Исправления по дизайну админки

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -9,12 +9,13 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0 mb-3">Посты</h1>
+                    <h1 class="m-0 mb-3">Мероприятия</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="/">На сайт</a></li>
                         <li class="breadcrumb-item"><a href="{{ route('admin.main.index') }}">Главная</a></li>
-                        <li class="breadcrumb-item active">Посты</li>
+                        <li class="breadcrumb-item active">Мероприятия</li>
                     </ol>
                 </div><!-- /.col -->
                 <div class="col-2">
