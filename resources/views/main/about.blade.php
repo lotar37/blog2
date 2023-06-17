03ade284b2bc80d564cf741e099a99aa4a2a454f
uncleA <lotar73@gmail.com>
2023-06-17 08:41:58 +0300

    Выпускники в about 2

--- resources/views/main/about.blade.php
+++ resources/views/main/about.blade.php
@@ -107,18 +107,18 @@
                     </section>
                     <section class="edica-about-clients mb-5">
                         <h2 class="clients-title mb-5" data-aos="fade-up">Наши выпускники</h2>
-                        <div class="row py-4 d-flex" data-aos="fade-up">
-                            <div class="col-2 mb-4 mb-md-0">
-                                <img src="assets/images/out.jpg" alt="client logo" style="border-radius:0.5rem;height: 160px;max-width: 100%">
+                        <div class="row py-4 d-flex justify-content-sm-around" data-aos="fade-up">
+                            <div class="mb-4 mb-md-0">
+                                <img src="assets/images/out.jpg" alt="client logo" style="border-radius:0.5rem;height: 190px;max-width: 100%">
                             </div>
-                            <div class="col-4  mb-4 mb-md-0">
-                                <img src="assets/images/out21.jpg" alt="client logo" style="border-radius:0.5rem;height: 160px;max-width: 100%">
+                            <div class="mb-4 mb-md-0">
+                                <img src="assets/images/out21.jpg" alt="client logo" style="border-radius:0.5rem;height: 190px;max-width: 100%">
                             </div>
-                            <div class="col-4 mb-4 mb-md-0">
-                                <img src="assets/images/out20.jpg" alt="client logo"  style="border-radius:0.5rem;height: 160px;max-width: 100%">
+                            <div class="mb-4 mb-md-0">
+                                <img src="assets/images/out20.jpg" alt="client logo"  style="border-radius:0.5rem;height: 190px;max-width: 100%">
                             </div>
-                         <div class="col-2 mb-4 mb-md-0">
-                                <img src="assets/images/out19.jpg" alt="client logo"  style="border-radius:0.5rem;height: 160px;max-width: 100%">
+                         <div class="mb-4 mb-md-0">
+                                <img src="assets/images/out19.jpg" alt="client logo"  style="border-radius:0.5rem;height: 190px;max-width: 100%">
                             </div>
 {{--                            <div class="col-6 col-md-2 mb-4 mb-md-0">--}}
 {{--                                <img src="assets/images/out.jpg" alt="client logo" width="140px" class="img-fluid">--}}
