17813eef2cda26b601bc1edf0aa58623f2ce1dfb
uncleA <lotar73@gmail.com>
2023-10-18 16:22:51 +0300

    метрика Яндекс

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -188,6 +188,25 @@
                     <a href="#!"><i class="fab fa-telegram fa-2x"></i></i></a>
                     <a href="https://vk.com/gymnasiaradonezh"><i class="fab fa-vk  fa-2x"></i></a>
                 </nav>
+                <p>
+                    <!-- Yandex.Metrika counter -->
+                    <script type="text/javascript" >
+                        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                            m[i].l=1*new Date();
+                            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                        ym(95098872, "init", {
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true,
+                            webvisor:true
+                        });
+                    </script>
+                    <noscript><div><img src="https://mc.yandex.ru/watch/95098872" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+                <!-- /Yandex.Metrika counter -->
+                </p>
             </div>
             <div class="col-md-3">
                 <nav class="footer-nav">
