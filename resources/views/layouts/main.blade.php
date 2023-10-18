bd2682646f1a656b2fd97661c6c926f3f8632ab7
uncleA <lotar73@gmail.com>
2023-10-18 19:59:10 +0300

    титл

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <title>Православная классическая гимназия "Радонеж"</title>
 {{--    <link rel="stylesheet" href="{{ asset("css/app.css") }}">--}}
     <link rel="stylesheet" href="{{ asset("assets/vendors/flag-icon-css/css/flag-icon.min.css") }}">
     <link rel="stylesheet" href="{{ asset("assets/vendors/font-awesome/css/all.min.css") }}">
