892bc7d2eb923361af8ea66b0c83a60e63700094
uncleA <lotar73@gmail.com>
2024-09-12 09:50:56 +0300

    Убрать страницы с пустыми роутами

--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -3,65 +3,72 @@
 @section('content')
 
     <!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0 mb-3">Страницы</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ route('admin.main.index') }}"><i class="nav-icon fas fa-home"></i></a></li>
-                        <li class="breadcrumb-item active">Посты</li>
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 mb-3">Страницы</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ route('admin.main.index') }}"><i
+                                        class="nav-icon fas fa-home"></i></a></li>
+                            <li class="breadcrumb-item active">Посты</li>
 
-                </div><!-- /.col -->
-                <div class="col-2">
-                    <a class="btn btn-block btn-outline-secondary" href="{{route("admin.page.create")}}">
-                                Добавить
-                    </a>
-                </div>
+                    </div>
+                    <div class="col-2">
+                        <a class="btn btn-block btn-outline-secondary" href="{{route("admin.page.create")}}">
+                            Добавить
+                        </a>
+                    </div>
 
-            </div><!-- /.row -->
-            <div class="row">
-                <div class="w-100">
-                    <div class="card">
-                        <!-- /.card-header -->
-                        <div class="card-body table-responsive p-0">
-                            <table class="table table-hover text-nowrap">
-                                <thead>
-                                <tr>
-                                    <th>В меню</th>
-                                    <th>Название страницы</th>
-                                    <th></th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($pages as $page)
-                                <tr>
-{{--                                    <td>{{ $page->id }}</td>--}}
-                                    <td>{{ $page->content }}</td>
-                                    <td><a href="{{ route("main.".$page->route_name) }}">{{ $page->title }}</a></td>
-                                    <td class="d-flex">
-                                        <a href="{{ route("main.".$page->route_name) }}" class="btn btn-outline-secondary mr-1">Перейти</a>
-                                        <a href="{{ route("admin.page.edit", $page->id) }}" class="btn btn-outline-secondary">Изменить</a>
-                                    </td>
-                                </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
+                </div><!-- /.row -->
+                <div class="row">
+                    <div class="w-100">
+                        <div class="card">
+                            <!-- /.card-header -->
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover text-nowrap">
+                                    <thead>
+                                    <tr>
+                                        <th>В меню</th>
+                                        <th>Название страницы</th>
+                                        <th></th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($pages as $page)
+                                        @if($page->route_name)
+                                            <tr>
+                                                {{--                                    <td>{{ $page->id }}</td>--}}
+                                                <td>{{ $page->content }}</td>
+                                                <td>
+                                                    <a href="{{ route("main.".$page->route_name) }}">{{ $page->title }}</a>
+                                                </td>
+                                                <td class="d-flex">
+                                                    <a href="{{ route("main.".$page->route_name) }}"
+                                                       class="btn btn-outline-secondary mr-1">Перейти</a>
+                                                    <a href="{{ route("admin.page.edit", $page->id) }}"
+                                                       class="btn btn-outline-secondary">Изменить</a>
+                                                </td>
+                                            </tr>
+                                        @endif
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
                         </div>
-                        <!-- /.card-body -->
                     </div>
                 </div>
-            </div>
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
 
-    <!-- Main content -->
-    <!-- /.content -->
-</div>
-<!-- /.content-wrapper -->
+        <!-- Main content -->
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
 @endsection
