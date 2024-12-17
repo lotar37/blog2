91243b8a94ce974f0204b17867ae3fdf5b4cc95d
adminn <lotar73@gmail.com>
2024-12-17 11:26:26 +0300

    post service проблема с intervention

--- app/Service/PostService.php
+++ app/Service/PostService.php
@@ -15,37 +15,37 @@ class PostService
             $tagg = false;
             DB::beginTransaction();
 
-            if($data["project_id"] == 0){
-                $data["project_id"] =  null;
+            if ($data["project_id"] == 0) {
+                $data["project_id"] = null;
             }
 
-            if(isset($data["tag_ids"])){
+            if (isset($data["tag_ids"])) {
                 $tags_data = $data["tag_ids"];
                 $tagg = true;
                 unset($data["tag_ids"]);
             }
             $data["date"] = Carbon::parse($data["date"])->format('Y-m-d');
-            if(isset($data['preview_image'])) {
+            if (isset($data['preview_image'])) {
                 $data['preview_image'] = Storage::disk("public")->put("/images", $data['preview_image']);
             }
 
-            if(isset($data['mainpage_image'])){
+            if (isset($data['mainpage_image'])) {
                 $data['mainpage_image'] = Storage::disk("public")->put("/images", $data['mainpage_image']);
             }
 
-            if(isset($data['mainpage'])){
+            if (isset($data['mainpage'])) {
                 $data['mainpage'] = '1';
-            }else{
+            } else {
                 $data['mainpage'] = '0';
             }
 
-            if(isset($data['main_image'])) {
+            if (isset($data['main_image'])) {
                 $data['main_image'] = Storage::disk("public")->put("/images", $data['main_image']);
             }
 //            dd($data);
             $post = Post::firstOrCreate($data);
 
-            if($tagg) {
+            if ($tagg) {
                 $post->tags()->attach($tags_data);
             }
             DB::commit();
@@ -63,33 +63,33 @@ class PostService
         try {
             DB::beginTransaction();
 
-            if($data["project_id"] == 0){
-                $data["project_id"] =  null;
+            if ($data["project_id"] == 0) {
+                $data["project_id"] = null;
             }
 
-            if(isset($data["tag_ids"])) {
+            if (isset($data["tag_ids"])) {
                 $tags_data = $data["tag_ids"];
                 unset($data["tag_ids"]);
             }
 
-            if(isset($data['mainpage_image'])){
+            if (isset($data['mainpage_image'])) {
                 $data['mainpage_image'] = Storage::disk("public")->put("/images", $data['mainpage_image']);
             }
-            if(isset($data['preview_image'])){
+            if (isset($data['preview_image'])) {
                 $data['preview_image'] = Storage::disk("public")->put("/images", $data['preview_image']);
             }
-            if(isset($data['main_image'])) {
+            if (isset($data['main_image'])) {
                 $data['main_image'] = Storage::disk("public")->put("/images", $data['main_image']);
             }
             $data["date"] = Carbon::parse($data["date"])->format('Y-m-d');
 
-            if(isset($data['mainpage'])){
+            if (isset($data['mainpage'])) {
                 $data['mainpage'] = '1';
-            }else{
+            } else {
                 $data['mainpage'] = '0';
             }
             $post->update($data);
-            if(isset($tags_data)) {
+            if (isset($tags_data)) {
                 $post->tags()->sync($tags_data);
             }
             DB::commit();
