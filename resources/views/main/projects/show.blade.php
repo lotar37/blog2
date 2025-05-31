cd8ecca1ee80dbf17e91685b0a60d9802e5b02e7
Home comp <lotar37@gmail.com>
2025-05-31 20:00:24 +0300

    страница проекта изменения макета и вывод до 12 мероприятий

--- resources/views/main/projects/show.blade.php
+++ resources/views/main/projects/show.blade.php
@@ -16,18 +16,23 @@
                 <div class="col-sm-4 p-0"><img src="{{ asset("storage/".$project->avatar)}}" alt="" style="border-radius: 0rem 0 0  0rem;max-width: 100%"></div>
                 <div class="col-sm-8"><h1 class="edica-page-title" data-aos="fade-up">{{ $project->name }}</h1></div>
             </div>
+            <section class="edica-landing-section edica-landing-blog">
+                <div class="container">
+                    <h4 class="edica-landing-section-subtitle-alt">ОПИСАНИЕ ПРОЕКТА</h4>
+                <div class="row">
+                    <div class="col-sm-12">{!! nl2br($project->description) !!}  </div>
+                </div>
+            </section>
             <section class="edica-landing-section edica-landing-blog" style="{{ count($project->posts) == 0 ? "display:none" : "" }}">
                 <div class="container">
                     <h4 class="edica-landing-section-subtitle-alt" >ПОСЛЕДНИЕ НОВОСТИ ПРОЕКТА</h4>
-                    {{--                    <h4 class="edica-landing-section-subtitle" data-aos="fade-up">События</h4>--}}
-                    {{--                    <h2 class="edica-landing-section-title" data-aos="fade-up">События </h2>--}}
                     <div class="row">
                         @php
                             $a = ["right","up","left"];
                             $i = 0;
                         @endphp
                         @foreach($project->posts as $post)
-                            <div class="col-md-4 landing-blog-post" data-aos="fade-{{ $a[$i]}}">
+                            <div class="col-md-4 landing-blog-post" data-aos="fade-{{ $a[$i%3] }}">
                                 @php $i++ @endphp
                                 <img src="{{ asset("storage/" . $post->main_image) }}" alt="blog post"
                                      class="blog-post-thumbnail" style="object-fit: cover;height: 150pt;">
@@ -38,17 +43,7 @@
                             </div>
                         @endforeach
                     </div>
-{{--                    <div class="col-lg-12 justify-content-lg-around flex-column"--}}
-{{--                         style="display: flex; justify-content: space-around">--}}
-{{--                        <a href="{{ route("main.post") }}" class="btn btn-success btn-lg my-4">ВСЕ НОВОСТИ</a>--}}
-{{--                    </div>--}}
-                </div>
-            </section>
-            <section class="edica-landing-section edica-landing-blog">
-                <div class="container">
-                    <h4 class="edica-landing-section-subtitle-alt">ОПИСАНИЕ ПРОЕКТА</h4>
-                <div class="row">
-                    <div class="col-sm-12">{!! nl2br($project->description) !!}  </div>
+
                 </div>
             </section>
             <section class="post-content">
